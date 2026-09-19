eaba36005ee25ed5139f0b0c50db8259b89a7964
mptechlab-lab <manuelpegoretti9270@icloud.com>
2026-09-19 23:56:05 +0200

    Add files via upload

---
+++ FS25_TrueTransportLife/scripts/TrueTransportLife.lua
@@ -0,0 +1,3 @@
+TrueTransportLife = {MOD_NAME = g_currentModName}
+if TrueLifeCore ~= nil and TrueLifeCore.registerModule ~= nil then TrueLifeCore.registerModule("transport", "0.1.0", {"transportStressProvider"}) end
+print("[TrueTransportLife] Loaded")
