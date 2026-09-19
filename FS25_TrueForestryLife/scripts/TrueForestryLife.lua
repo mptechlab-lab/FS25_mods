b0617c751984d69ce7eff3680119187ddbca018b
mptechlab-lab <manuelpegoretti9270@icloud.com>
2026-09-19 23:54:53 +0200

    Add files via upload

---
+++ FS25_TrueForestryLife/scripts/TrueForestryLife.lua
@@ -0,0 +1,3 @@
+TrueForestryLife = {MOD_NAME = g_currentModName}
+if TrueLifeCore ~= nil and TrueLifeCore.registerModule ~= nil then TrueLifeCore.registerModule("forestry", "0.1.0", {"forestryStressProvider"}) end
+print("[TrueForestryLife] Loaded")
