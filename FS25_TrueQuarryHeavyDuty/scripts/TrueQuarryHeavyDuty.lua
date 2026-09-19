7b11298b1a857df988525109fc2f1807f6bfda3c
mptechlab-lab <manuelpegoretti9270@icloud.com>
2026-09-19 23:55:42 +0200

    Add files via upload

---
+++ FS25_TrueQuarryHeavyDuty/scripts/TrueQuarryHeavyDuty.lua
@@ -0,0 +1,3 @@
+TrueQuarryHeavyDuty = {MOD_NAME = g_currentModName}
+if TrueLifeCore ~= nil and TrueLifeCore.registerModule ~= nil then TrueLifeCore.registerModule("quarry", "0.1.0", {"heavyDutyStressProvider"}) end
+print("[TrueQuarryHeavyDuty] Loaded")
